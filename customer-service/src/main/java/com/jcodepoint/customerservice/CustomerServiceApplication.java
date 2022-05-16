84f9fc85cadd2d5472fe4acf9b335b8d5c08a57e
jcodepoint <jcodepoint@gmail.com>
2022-05-16 20:29:41 -0300

    Commit.

---
+++ customer-service/src/main/java/com/jcodepoint/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jcodepoint.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
