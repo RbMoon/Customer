89d822a3d7187313528a2dc767e65252358f7c29
jcodepoint <jcodepoint@gmail.com>
2022-05-18 13:13:42 -0300

    Commit.

---
+++ customer-service-swagger/src/main/java/com/jcodepoint/customerserviceswagger/CustomerServiceSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.jcodepoint.customerserviceswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceSwaggerApplication.class, args);
+	}
+
+}
